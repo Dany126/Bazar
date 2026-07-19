80d4d3e9c5068c3a0adbb59f0b00175e6773810b
Mohamed12adel <162705350+Mohamed12adel@users.noreply.github.com>
2026-07-20 02:54:39 +0300

    backend files structure and authentication

---
+++ e_commerce/server/models/user_model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    min: 11,
+  },
+  password_hash: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
