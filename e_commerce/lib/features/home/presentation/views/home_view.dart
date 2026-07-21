3f8e325044e28e7ea98d9d3a460df334aabe6c08
Dany Ashraf <danyashraf12@gmail.com>
2026-07-21 17:50:54 +0300

    create home page file

---
+++ e_commerce/lib/features/home/presentation/views/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+  static const routeName = 'HomeView';
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder(
+      builder: (context, state) {
+        return const HomeViewBody();
+      },
+    );
+  }
+}
