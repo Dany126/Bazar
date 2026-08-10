3177fa2a7fd1a5e39d610d7c84ad4cc394f62bee
Dany Ashraf <danyashraf12@gmail.com>
2026-08-10 19:16:55 +0300

    edit comments

--- e_commerce/server/routes/notification_routes.js
+++ e_commerce/server/routes/notification_routes.js
@@ -10,13 +10,11 @@ export const notificationRouter = express.Router();
 
 notificationRouter.route("/").get(getAllNotifications);
 
-<<<<<<< HEAD
 notificationRouter.route("/:id").delete(checkID, deleteNotification);
 
 
-=======
+
 notificationRouter
   .route("/:id")
   .delete(checkID, deleteNotification)
   .patch(checkID, updateNotification);
->>>>>>> 7c297722b87d19fb5b8dbb8a1a982975301bcc83
