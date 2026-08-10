b8ce056cbbae842ea9d46b0ac868202df8e3f41c
Mohamed12adel <162705350+Mohamed12adel@users.noreply.github.com>
2026-08-10 15:09:20 +0300

    fix: correct token version check in requireAuth middleware

--- e_commerce/server/middleware/requireAuth.js
+++ e_commerce/server/middleware/requireAuth.js
@@ -17,12 +17,12 @@ export async function requireAuth(req, res, next) {
         message: "user not found",
       });
     }
-    console.log(user.tokenVersion, payload.tokenVersion);
-    // if (user?.tokenVersion !== payload.tokenversion) {
-    //   return res.status(401).json({
-    //     message: "Token invalid",
-    //   });
-    // }
+
+    if (user?.tokenVersion !== payload.tokenversion) {
+      return res.status(401).json({
+        message: "Token invalid",
+      });
+    }
 
     req.user = {
       id: user?.id,
